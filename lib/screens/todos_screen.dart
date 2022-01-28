056488dbd8fbcb746a45ddb270a8df71cdf3bb34
Mai Thinh <maihophucthinh@gmail.com>
2022-01-28 13:50:03 +0700

    test github actions 3

--- lib/screens/todos_screen.dart
+++ lib/screens/todos_screen.dart
@@ -46,7 +46,7 @@ class TodosScreen extends StatelessWidget {
                         ),
                       );
                     })
-                : const Center(child: Text('Test github actions 2')),
+                : const Center(child: Text('Test github actions 3')),
             floatingActionButton: FloatingActionButton(
               onPressed: () => navigateToCreateTodoScreen(context),
               child: const Icon(Icons.add),
